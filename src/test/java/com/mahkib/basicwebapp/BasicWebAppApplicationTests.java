7e3af3666426bcdaea821f5ad4195459e4faa944
bumahkib7 <93518580+bumahkib7@users.noreply.github.com>
2022-06-09 00:34:17 +0200

    initial commit

---
+++ src/test/java/com/mahkib/basicwebapp/BasicWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahkib.basicwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

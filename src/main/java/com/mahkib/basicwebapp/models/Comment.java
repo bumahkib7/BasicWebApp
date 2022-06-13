72e6850764860328d672cf7388fe18b6183a3267
bumahkib7 <93518580+bumahkib7@users.noreply.github.com>
2022-06-13 19:53:58 +0200

    Controllers and tests

--- src/main/java/com/mahkib/basicwebapp/models/Comment.java
+++ src/main/java/com/mahkib/basicwebapp/models/Comment.java
@@ -1,6 +1,10 @@
 package com.mahkib.basicwebapp.models;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -8,11 +12,15 @@ import org.yaml.snakeyaml.comments.CommentType;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.Objects;
 
 @Entity
-@Table(name = "comment")
-@EntityListeners({ AuditingEntityListener.class })
-@Data
+@Table
+@EntityListeners({ AuditingEntityListener.class})
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class Comment {
 
     @Id
@@ -30,6 +38,18 @@ public class Comment {
     @CreatedBy
     private String createdBy;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Comment comment = (Comment) o;
+        return id != null && Objects.equals(id, comment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
 
 // End of file Comment.java

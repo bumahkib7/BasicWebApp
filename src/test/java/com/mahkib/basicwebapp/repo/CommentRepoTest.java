1f1cc947bad9dbcaf0eb62f889ad09cddb219e60
bumahkib7 <93518580+bumahkib7@users.noreply.github.com>
2022-06-13 11:13:06 +0200

    Services tests

--- src/test/java/com/mahkib/basicwebapp/repo/CommentRepoTest.java
+++ src/test/java/com/mahkib/basicwebapp/repo/CommentRepoTest.java
@@ -3,6 +3,7 @@ package com.mahkib.basicwebapp.repo;
 import com.mahkib.basicwebapp.models.Comment;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +19,13 @@ import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-@AllArgsConstructor
-@NoArgsConstructor
+
 
 public class CommentRepoTest {
 
@@ -33,51 +35,32 @@ public class CommentRepoTest {
     @Autowired
     private CommentRepo commentRepo;
 
-
-
-    @Test
+  @Test
     public void findByCreatedYearAndMonthAndDay_HappyPath_ShouldReturn1Comment() {
-        // Given
-        Comment comment = new Comment();
-        comment.setComment("This is a comment");
-        comment.setType(CommentType.IN_LINE);
-        comment.setCreatedDate(new Timestamp(System.currentTimeMillis()));
-        testEntityManager.persist(comment);
-        testEntityManager.flush();
+      // Given
+      Comment comment = new Comment();
+      comment.setComment("This is a comment");
+      comment.setType(CommentType.IN_LINE);
+      comment.setCreatedDate(new Timestamp(System.currentTimeMillis()));
+      List<Comment> comments = List.of(comment);
 
-        //When
-        LocalDate localDate = LocalDate.now();
-        List<Comment> comments =
-                commentRepo.findByCreatedYearAndMonthAndDay(localDate.getYear(),
-                        localDate.getMonthValue(),
-                        localDate.getDayOfMonth()
-                );
+      LocalDate now = LocalDate.now();
+      when(commentRepo.findByCreatedYearAndMonthAndDay(now.getYear(),
+              now.getMonthValue(),
+              now.getDayOfMonth())).thenReturn(comments);
 
+      // When
+      List<Comment> actualComments = commentRepo.findByCreatedYearAndMonthAndDay(now.getYear(),
+              now.getMonthValue(),
+              now.getDayOfMonth());
 
-        //Then
-        assertThat(comments.size(), is(1));
-        assertThat(comments.get(0).getComment(), is("This is a comment"));
+      // Then
+      verify(commentRepo).findByCreatedYearAndMonthAndDay(now.getYear(),
+              now.getMonthValue(),
+              now.getDayOfMonth());
+      assertThat(comments, is(actualComments));
 
-
-    }
-
-
-
-    @Test
-    public void save_HappyPath_shouldSave1Comment() {
-        //Given
-        Comment comment = new Comment();
-        comment.setComment("This is a Test Comment");
-        comment.setType(CommentType.IN_LINE);
-        comment.setCreatedDate(new Timestamp(System.currentTimeMillis()));
-
-
-        //When
-        Comment savedComment = commentRepo.save(comment);
-
-        //Then
-        assertThat(testEntityManager.find(Comment.class, savedComment.getId()), is(savedComment));
-    }
+  }
 
 
 }
